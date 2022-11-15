25b67b7349a09221309a7ac938125884b22ef304
dhirush <88309520+dhirush@users.noreply.github.com>
2022-11-15 06:55:22 +0530

    Add files via upload

---
+++ target.py
@@ -0,0 +1,141 @@
+import shutil
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning Sir !")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon Sir !")
+
+    else:
+        speak("Good Evening Sir !")
+
+    assname = ("Jarvis 1 point o")
+    speak("I am your Assistant")
+    speak(assname)
+
+
+def username():
+    speak("What should i call you sir")
+    uname = takeCommand()
+    speak("Welcome Mister")
+    speak(uname)
+    columns = shutil.get_terminal_size().columns
+
+    print("#####################".center(columns))
+    print("Welcome Mr.", uname.center(columns))
+    print("#####################".center(columns))
+
+    speak("How can i Help you, Sir")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Unable to Recognize your voice.")
+        return "None"
+
+    return query
+
+
+if __name__ == '__main__':
+    clear = lambda: os.system('cls')
+
+    # This Function will clean any
+    # command before execution of this python file
+    clear()
+    wishMe()
+    username()
+
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            speak("Here you go to Youtube\n")
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            speak("Here you go to Google\n")
+            webbrowser.open("google.com")
+
+        elif 'open stack overflow' in query:
+            speak("Here you go to Stack Over flow.Happy coding")
+            webbrowser.open("stackoverflow.com")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("% H:% M:% S")
+            speak(f"Sir, the time is {strTime}")
+        elif 'how are you' in query:
+            speak("I am fine, Thank you")
+            speak("How are you, Sir")
+
+        elif 'fine' in query or "good" in query:
+            speak("It's good to know that your fine")
+
+        elif "change my name to" in query:
+            query = query.replace("change my name to", "")
+            assname = query
+
+        elif "change name" in query:
+            speak("What would you like to call me, Sir ")
+            assname = takeCommand()
+            speak("Thanks for naming me")
+
+        elif "what's your name" in query or "What is your name" in query:
+            speak("My friends call me")
+            speak(assname)
+            print("My friends call me", assname)
+
+        elif 'exit' in query:
+            speak("Thanks for giving me your time")
+            exit()
+
+        elif "who made you" in query or "who created you" in query:
+            speak("I have been created by Dhruv.")
+        elif "don't listen" in query or "stop listening" in query:
+            speak("for how much time you want to stop jarvis from listening commands")
+            a = int(takeCommand())
+            time.sleep(a)
+            print(a)
+        elif "btech" and "sem5" and "material" in query:
+            webbrowser.open_new_tab("https://drive.google.com/drive/folders/1gnHVGhEhPwQ816aFhpJ4Gk2H5U3yLUc7")
+        else:
+            speak("not understand your command")
